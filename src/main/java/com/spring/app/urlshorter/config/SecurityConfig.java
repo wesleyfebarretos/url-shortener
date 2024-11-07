43511b8f897bc05b838651ea8c226cfe515d5f83
Wesley Ferreira <wesleyfebarretos@gmail.com>
2024-11-07 19:42:18 -0300

    feat: allow permit all in integration route prefix

--- src/main/java/com/spring/app/urlshorter/config/SecurityConfig.java
+++ src/main/java/com/spring/app/urlshorter/config/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
                                 "/swagger-resources",
                                 "/v3/api-docs",
                                 "/v3/api-docs/**",
-                                "/v2/api-docs/**"
+                                "/v2/api-docs/**",
+                                "/integration/**"
                         ).permitAll()
                         .requestMatchers(HttpMethod.POST, "/user").permitAll()
                         .anyRequest().authenticated()
