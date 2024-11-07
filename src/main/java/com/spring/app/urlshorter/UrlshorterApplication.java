f3ca03d74c41778ddd5119cd3414f887f2c6c74c
Wesley Ferreira <wesleyfebarretos@gmail.com>
2024-11-07 19:42:31 -0300

    feat: enable feign clients

--- src/main/java/com/spring/app/urlshorter/UrlshorterApplication.java
+++ src/main/java/com/spring/app/urlshorter/UrlshorterApplication.java
@@ -3,8 +3,10 @@ package com.spring.app.urlshorter;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UrlshorterApplication {
 
 	public static void main(String[] args) {
