bbd45fb59b8ca1f80f9534e3d739e2f1ff18c353
Wesley Ferreira <wesleyfebarretos@gmail.com>
2024-11-07 19:42:53 -0300

    feat: jdbc implementation

--- src/main/java/com/spring/app/urlshorter/repository/CustomUrlRepositoryImpl.java
+++ src/main/java/com/spring/app/urlshorter/repository/CustomUrlRepositoryImpl.java
@@ -1,36 +1,59 @@
 package com.spring.app.urlshorter.repository;
 
 import com.spring.app.urlshorter.entity.UrlEntity;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.time.ZoneId;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class CustomUrlRepositoryImpl implements CustomUrlRepository {
-    @PersistenceContext
-    private EntityManager entityManager;
+    private static final String query = """
+                UPDATE url 
+                SET 
+                    access_qty = access_qty + 1 
+                WHERE 
+                    short_code = :shortcode 
+                AND
+                    expiration_at > now()
+                RETURNING *
+            """;
+
+    private final NamedParameterJdbcTemplate jdbc;
+    //  A different way to pass params as Array and not by hash map named params
+    //    private final JdbcTemplate jdbcTemplate;
 
     @Override
     public Optional<UrlEntity> updateAndFindByCode(String shortcode) {
         try {
-            String sql = """
-                        UPDATE url 
-                        SET 
-                            access_qty = access_qty + 1 
-                        WHERE 
-                            short_code = :shortcode 
-                        AND
-                            expiration_at > now()
-                        RETURNING *
-                    """;
+            Map<String, Object> params = new HashMap<>();
 
-            Query query = entityManager.createNativeQuery(sql, UrlEntity.class);
-            query.setParameter("shortcode", shortcode);
+            params.put("shortcode", shortcode);
 
-            return Optional.ofNullable((UrlEntity) query.getSingleResult());
+            UrlEntity url = jdbc.query(query, params, (row) -> {
+                if (row.next()) {
+                    return UrlEntity.builder()
+                            .id(row.getLong("id"))
+                            .shortCode(row.getString("short_code"))
+                            .originalAddress(row.getString("original_address"))
+                            .accessQty(row.getInt("access_qty"))
+                            .expirationAt(row.getTimestamp("expiration_at").toLocalDateTime().atZone(ZoneId.systemDefault()))
+                            .updatedAt(row.getTimestamp("updated_at").toLocalDateTime().atZone(ZoneId.systemDefault()))
+                            .createdAt(row.getTimestamp("created_at").toLocalDateTime().atZone(ZoneId.systemDefault()))
+                            .build();
+                }
+
+                return null;
+            });
+
+            return Optional.ofNullable(url);
         } catch (Exception e) {
             return Optional.empty();
         }
