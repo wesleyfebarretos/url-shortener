ae0d0e54c04e93929bba80b5f475f77ba60084cc
Wesley Ferreira <wesleyfebarretos@gmail.com>
2024-11-13 17:41:15 -0300

    feat: thread example

---
+++ src/test/java/com/spring/app/urlshorter/unit/ThreadTests.java
@@ -0,0 +1,82 @@
+package com.spring.app.urlshorter.unit;
+
+import org.apache.groovy.util.ObjectHolder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
+@ExtendWith(MockitoExtension.class)
+public class ThreadTests {
+    private final Object obj = new Object();
+    private final Object obj2 = new Object();
+
+    @Test
+    @DisplayName("thread test")
+    public void testSyncronized() {
+        var threads = IntStream.rangeClosed(1, 100).mapToObj((i) -> {
+            return new Thread(() -> {
+//                printI(i);
+//                printWithLock(i);
+                printWithLock2(i);
+            });
+        }).map((thread) -> {
+            thread.start();
+            return thread;
+        }).toList();
+
+        threads.forEach((thread) -> {
+            try {
+                thread.join();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        });
+    }
+
+    public synchronized void printI(int i) {
+        try {
+            System.out.println(i);
+            Thread.sleep((long) (Math.random() * 100));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void printWithLock(int i) {
+        synchronized (obj) {
+            try {
+                System.out.println("lock 1 -> " + i);
+                Thread.sleep((long) (Math.random() * 500));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+        synchronized (obj2) {
+            try {
+                System.out.println("lock 2 -> " + i);
+                Thread.sleep((long) (Math.random() * 500));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    ReentrantLock lock = new ReentrantLock();
+
+    public void printWithLock2(int i) {
+        lock.lock();
+        try {
+            System.out.println("lock 2 -> " + i);
+//            Thread.sleep((long) (Math.random() * 10));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        lock.unlock();
+    }
+}
